24e183d6523515acf312b76d4288e573d2f305d9
Ken <lax.potcx1562@gmail.com>
2025-07-22 21:37:34 +0900

    シリアライズに関する設定追加

---
+++ ktor-apigateway-core/src/main/kotlin/jp/lax256/apigateway/core/util/ApiGatewayPayloadJson.kt
@@ -0,0 +1,25 @@
+package jp.lax256.apigateway.core.util
+
+import jp.lax256.apigateway.core.contract.dto.Firebase
+import jp.lax256.apigateway.core.contract.dto.FirebaseAppleAuth
+import jp.lax256.apigateway.core.contract.dto.FirebaseFacebookAuth
+import jp.lax256.apigateway.core.contract.dto.FirebaseGoogleAuth
+import jp.lax256.apigateway.core.contract.dto.FirebasePasswordAuth
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+
+val ApiGatewayPayloadJson = Json {
+    ignoreUnknownKeys = true
+    prettyPrint = true
+    classDiscriminator = "sign_in_provider"
+    serializersModule = SerializersModule {
+        polymorphic(Firebase::class) {
+            subclass(FirebasePasswordAuth::class)
+            subclass(FirebaseAppleAuth::class)
+            subclass(FirebaseGoogleAuth::class)
+            subclass(FirebaseFacebookAuth::class)
+        }
+    }
+}
