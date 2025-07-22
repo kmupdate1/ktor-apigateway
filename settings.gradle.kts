391ac31d361e307c2b2599a959f109fb050e7227
Ken <lax.potcx1562@gmail.com>
2025-07-23 02:20:09 +0900

    一部修正

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,18 @@
 pluginManagement {
     plugins {
         kotlin("jvm") version "2.2.0"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    }
+    repositories {
+        gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
+
 rootProject.name = "ktor-apigateway"
 
 include(
     ":ktor-apigateway-core",
     ":ktor-apigateway-gcp",
     ":ktor-apigateway-aws",
-    ":ktor-apigateway-azure",
+    ":ktor-apigateway-azure"
 )
