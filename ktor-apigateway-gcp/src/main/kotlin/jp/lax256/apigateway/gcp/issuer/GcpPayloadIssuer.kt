f46614253c5362c8f4d02d3ceafaed6c971eb7e8
Ken <lax.potcx1562@gmail.com>
2025-07-24 17:45:48 +0900

    ログ出力内容変更

--- ktor-apigateway-gcp/src/main/kotlin/jp/lax256/apigateway/gcp/issuer/GcpPayloadIssuer.kt
+++ ktor-apigateway-gcp/src/main/kotlin/jp/lax256/apigateway/gcp/issuer/GcpPayloadIssuer.kt
@@ -10,7 +10,7 @@ import java.util.*
 class GcpPayloadIssuer: PayloadIssuer {
     override fun issueFromHeader(call: ApplicationCall): JwtPayload {
         val xApigatewayApiUserinfo = call.request.headers[GatewayHttpHeaders.XApigatewayApiUserinfo]
-            ?: throw IllegalStateException("${GatewayHttpHeaders.XApigatewayApiUserinfo} header not found.").apply {
+            ?: throw IllegalStateException("[${GatewayHttpHeaders.XApigatewayApiUserinfo}] header not found.").apply {
                 call.application.log.error(this.message, this)
             }
 
@@ -18,17 +18,17 @@ class GcpPayloadIssuer: PayloadIssuer {
             Base64.getUrlDecoder().decode(xApigatewayApiUserinfo)
                 .let { String(it, Charsets.UTF_8) }
                 .run {
-                    call.application.log.info("Decoded ${GatewayHttpHeaders.XApigatewayApiUserinfo} Payload: $this")
+                    call.application.log.info("Decoded [${GatewayHttpHeaders.XApigatewayApiUserinfo}] header.")
                     ApiGatewayPayloadJson.decodeFromString<JwtPayload>(this)
                 }
         }.fold(
             onSuccess = { jwtPayload ->
-                call.application.log.info("SUCCESS decoding and parsing ${GatewayHttpHeaders.XApigatewayApiUserinfo}: user_id - ${jwtPayload.userId}, email - ${jwtPayload.email}")
+                call.application.log.info("SUCCESS decoding and parsing [${GatewayHttpHeaders.XApigatewayApiUserinfo}] header.")
                 jwtPayload
             },
             onFailure = { throwable ->
                 call.application.log.error(
-                    "FAILED to decoding or parsing ${GatewayHttpHeaders.XApigatewayApiUserinfo}: ${throwable.message}",
+                    "FAILED to decoding or parsing [${GatewayHttpHeaders.XApigatewayApiUserinfo}] header.: ${throwable.message}",
                     throwable,
                 )
                 throw throwable
